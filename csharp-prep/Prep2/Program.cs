29a02c854eee90bf5da9b7d5b02b268da31ae127
Carter Mecham <cartermmecham@gmail.com>
2026-02-28 11:41:45 -0600

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
 
 class Program
 {
+
+   
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    { 
+        
+        Console.Write("enter a number: ");
+        string UserNumber = Console.ReadLine();
+        int x = int.Parse(UserNumber);
+        int y = 2;
+
+        if (x > y)
+        {
+            Console.WriteLine("greater");
+        }
+        else if(x < y)
+        {
+            Console.WriteLine("less");
+        }
     }
 }
